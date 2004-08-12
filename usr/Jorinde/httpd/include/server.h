38e626a37222769c896c3d3335105de4aca2e2dc
Joakim Romland <romland@gmail.com>
2004-08-12 09:21:52 +0000

    - Changelog updates. - Version to 0.3.14

--- usr/Jorinde/httpd/include/server.h
+++ usr/Jorinde/httpd/include/server.h
@@ -1,8 +1,8 @@
 /**
  * Description: Server specific headers.
- * Version:     $Id: server.h 255 2004-07-09 14:45:41Z romland $
+ * Version:     $Id$
  * License:     (c)2004 Joakim Romland, see doc/License
  */
 
-# define HTTPD_VERSION		"0.3.13"
+# define HTTPD_VERSION		"0.3.14"
 
